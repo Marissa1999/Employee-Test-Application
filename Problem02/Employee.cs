7c3755ff80c47f4b3961c47269e955f537f3bac8
Marissa Gonçalves <marissagoncalves13@gmail.com>
2021-01-05 13:59:49 -0500

    Initial Commit.

---
+++ Problem02/Employee.cs
@@ -0,0 +1,105 @@
+﻿//ID: 1775227
+//Name: Marissa Goncalves
+//Assignment 1 - Problem 2: EmployeeTest app
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem02
+{
+    class Employee
+    {
+
+        //Initialize the instance variables of the Employee class
+        private string firstName;
+        private string lastName;
+        private decimal monthlySalary;
+
+
+        //Main constructor for the Employee class that takes all inputted values in the parameter
+        public Employee(string firstName, string lastName, decimal monthlySalary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            MonthlySalary = monthlySalary;
+        }
+
+
+        //Implicit property for the first name
+        public string FirstName
+        {
+            get; set;
+        }
+
+
+        //Implicit property for the last name
+        public string LastName
+        {
+            get; set;
+        }
+
+
+
+
+        //Property for the monthly salary
+        public decimal MonthlySalary
+        {
+
+            get
+            {
+                //Return the monthly salary
+                return this.monthlySalary;
+            }
+
+            set
+            {
+                //If the value is less than 0.0, set the monthly salary to 0.0
+                if (value < 0.0m)
+                {
+                    this.monthlySalary = 0;
+                }
+
+                //Or else, the monthly salary is set to the value
+                else
+                {
+                    this.monthlySalary = value;
+                }
+
+            }
+
+
+        }
+
+
+        //Returns the monthly salary with a 10% raise
+        public decimal Raised10Percent()
+        {
+            return MonthlySalary *= 1.10m;
+        }
+
+
+
+        //Using string interpolation, display the employee record with the object values
+        public void DisplayEmployeeRecord()
+        {
+
+            Console.WriteLine($"------------------------------------------");
+            Console.WriteLine($"|            Employee Record             |");
+            Console.WriteLine($"------------------------------------------");
+            Console.WriteLine($"| First Name      |  {FirstName,-20}|");
+            Console.WriteLine($"------------------------------------------");
+            Console.WriteLine($"| Last Name       |  {LastName,-20}|");
+            Console.WriteLine($"------------------------------------------");
+            Console.WriteLine($"| Monthly Salary  |  {MonthlySalary,-20:C}|");
+            Console.WriteLine($"------------------------------------------");
+            Console.WriteLine();
+        }
+
+
+
+
+    }
+}
---
+++ Problem02/EmployeeTest.cs
@@ -0,0 +1,58 @@
+﻿//ID: 1775227
+//Name: Marissa Goncalves
+//Assignment 1 - Problem 2: EmployeeTest app
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem02
+{
+    class EmployeeTest
+    {
+        static void Main(string[] args)
+        {
+
+            //Create new objects with different values from the Employee class
+            Employee one = new Employee("Fred", "Andrews", 6258.80m);
+            Employee two = new Employee("Daniel", "Graham", 3698.34m);
+            Employee three = new Employee("Mike", "Frank", -320.23m);
+
+            //Skip a line
+            Console.WriteLine();
+
+            //Print a title before showing the initial employee records
+            Console.WriteLine("Initial Employee Records");
+
+            //Call each object to this method, to display the records in an organized table
+            one.DisplayEmployeeRecord();
+            two.DisplayEmployeeRecord();
+            three.DisplayEmployeeRecord();
+
+            //Skip a line
+            Console.WriteLine();
+
+
+            //Call each object in this method, to raise the monthly salary by 10%
+            one.Raised10Percent();
+            two.Raised10Percent();
+            three.Raised10Percent();
+
+
+            //Skip a line
+            Console.WriteLine();
+
+            //Print a title before showing the employee records after the 10% raise
+            Console.WriteLine("Employee Records After 10% Raise");
+
+
+            //Call each object to this method, to display the records in an organized table again to test the Raised10Percent() method
+            one.DisplayEmployeeRecord();
+            two.DisplayEmployeeRecord();
+            three.DisplayEmployeeRecord();
+
+        }
+    }
+}
